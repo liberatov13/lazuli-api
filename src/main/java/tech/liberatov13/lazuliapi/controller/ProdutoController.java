363c19f43c5228f664d88e712b9ca2883aa5a772
Elvis Liberato <liberatov13@gmail.com>
2024-02-25 16:24:57 -0300

    feat: criar listagem de produtos
    LBT-21: Desenvolvendo tela para listagem de produtos
    A tela também permitirá que o usuário ative e inative produtos diretamente da lista de produtos

--- src/main/java/tech/liberatov13/lazuliapi/controller/ProdutoController.java
+++ src/main/java/tech/liberatov13/lazuliapi/controller/ProdutoController.java
@@ -3,9 +3,7 @@ package tech.liberatov13.lazuliapi.controller;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.liberatov13.lazuliapi.domain.Produto;
 import tech.liberatov13.lazuliapi.dto.ProdutoDTO;
 import tech.liberatov13.lazuliapi.service.ProdutoService;
@@ -26,5 +24,17 @@ public class ProdutoController {
 		List<Produto> produtos = produtoService.findAll();
 		List<ProdutoDTO> response = produtos.stream().map(produto -> modelMapper.map(produto, ProdutoDTO.class)).toList();
 		return ResponseEntity.ok().body(response);
+	}
+
+	@PutMapping("/{idProduto}/desativar")
+	public ResponseEntity<Void> delete(@PathVariable Long idProduto) {
+		produtoService.disable(idProduto);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{idProduto}/ativar")
+	public ResponseEntity<Void> activate(@PathVariable Long idProduto) {
+		produtoService.activate(idProduto);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/tech/liberatov13/lazuliapi/service/ProdutoService.java
+++ src/main/java/tech/liberatov13/lazuliapi/service/ProdutoService.java
@@ -40,9 +40,7 @@ public class ProdutoService {
 
     public Produto save(Produto produto) {
         produto.setDescricaoBasica(produto.getDescricaoBasica().toUpperCase());
-        if (produto.getDescricaoCompleta().isEmpty()) {
-            produto.setDescricaoCompleta(null);
-        } else {
+        if (produto.getDescricaoCompleta() != null && !produto.getDescricaoCompleta().isBlank()) {
             produto.setDescricaoCompleta(produto.getDescricaoCompleta().toUpperCase());
         }
         Produto produtoSalvo = null;
@@ -50,8 +48,8 @@ public class ProdutoService {
             produtoSalvo = produtoRepository.save(produto);
             logger.info("Produto salvo com sucesso, id: " + produtoSalvo.getIdProduto());
         } catch (Exception e) {
-            // TODO: Avaliar a possibilidade de retornar uma Exception para o controller
-            logger.warn("Erro ao persistir produto", e);
+            logger.error("Ocorreu um erro ao persistir produto", e);
+            throw new RuntimeException("Erro ao persistir produto", e);
         }
         return produtoSalvo;
     }
@@ -74,6 +72,37 @@ public class ProdutoService {
 
     public Produto findById(Long idProduto) {
         return produtoRepository.findById(idProduto).orElseThrow(() -> new RuntimeException("Produto com id não" + idProduto + " encontrado"));
+    }
+
+    /**
+     * Realiza a ativação do produto
+     * @param idProduto Id do produto a ser ativado
+     * @return Produto ativado
+     */
+    public Produto activate(Long idProduto) {
+        try {
+            Produto produto = this.findById(idProduto);
+            produto.setStatus(true);
+            return produtoRepository.save(produto);
+        } catch (Exception e) {
+            logger.error("Ocorreu um erro ao ativar produtoo produto {}", idProduto, e);
+            throw new RuntimeException("Erro ao ativar o produto "+idProduto, e);
+        }
+    }
+
+	/**
+	 * Realiza a desativação do produto
+	 * @param idProduto  Id do produto a ser desativado
+	 */
+    public Produto disable(Long idProduto) {
+        try {
+            Produto produto = this.findById(idProduto);
+            produto.setStatus(false);
+            return produtoRepository.save(produto);
+        } catch (Exception e) {
+            logger.error("Ocorreu um erro ao desativar produtoo produto {}", idProduto, e);
+            throw new RuntimeException("Erro ao desativar o produto "+idProduto, e);
+        }
     }
 
 }

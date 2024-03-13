d63de0804ef664bd18aa05eb756d2d33bdf0a9e7
Elvis Liberato <liberatov13@gmail.com>
2024-03-12 23:52:41 -0300

    feat: incluir informações do swagger em ProdutoController

--- src/main/java/tech/liberatov13/lazuliapi/controller/ProdutoController.java
+++ src/main/java/tech/liberatov13/lazuliapi/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.liberatov13.lazuliapi.domain.Produto;
@@ -22,7 +23,11 @@ public class ProdutoController {
 
 	private final ModelMapper modelMapper = new ModelMapper();
 
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = {
+			@ApiResponse(description = "Produtos obtidos com sucesso.", responseCode = "200"),
+			@ApiResponse(description = "Erro ao obter produtos.", responseCode = "500")
+	})
 	public ResponseEntity<List<ProdutoDTO>> findAll() {
 		List<Produto> produtos = produtoService.findAll();
 		List<ProdutoDTO> response = produtos.stream().map(produto -> modelMapper.map(produto, ProdutoDTO.class)).toList();
@@ -30,6 +35,10 @@ public class ProdutoController {
 	}
 
 	@PostMapping
+	@ApiResponses(value = {
+			@ApiResponse(description = "Produto salvo com sucesso.", responseCode = "201"),
+			@ApiResponse(description = "Erro ao salvar produto.", responseCode = "500")
+	})
 	public ResponseEntity<ProdutoDTO> save(@RequestBody ProdutoDTO produtoDTO) {
 		Produto produto = modelMapper.map(produtoDTO, Produto.class);
 		Produto produtoSaved = produtoService.save(produto);
@@ -38,12 +47,20 @@ public class ProdutoController {
 	}
 
 	@PutMapping("/{idProduto}/desativar")
+	@ApiResponses(value = {
+			@ApiResponse(description = "Produto desativado com sucesso.", responseCode = "204"),
+			@ApiResponse(description = "Erro ao desativar produto.", responseCode = "500")
+	})
 	public ResponseEntity<Void> delete(@PathVariable Long idProduto) {
 		produtoService.disable(idProduto);
 		return ResponseEntity.noContent().build();
 	}
 
 	@PutMapping("/{idProduto}/ativar")
+	@ApiResponses(value = {
+			@ApiResponse(description = "Produto ativado com sucesso.", responseCode = "204"),
+			@ApiResponse(description = "Erro ao ativar produto.", responseCode = "500")
+	})
 	public ResponseEntity<Void> activate(@PathVariable Long idProduto) {
 		produtoService.activate(idProduto);
 		return ResponseEntity.noContent().build();
